9be6a80db706ed9f1718c6b05d8c422a8567a7bf
Darryl-Shi <221474B@student.hci.edu.sg>
2023-06-10 01:52:23 +0800

    rewrite in pytorch lightning :>

--- train.py
+++ train.py
@@ -1,59 +1,62 @@
-#%%
-import numpy as np
-import time
-import torch
-import torch.nn as nn
 import os
 import sys
+import torch
+import torch.nn as nn
 from tqdm import tqdm
+import pytorch_lightning as pl
 
 from config import Config
 from model import CSRNet
-from dataset import create_train_dataloader,create_test_dataloader
+from dataset import create_train_dataloader, create_test_dataloader
 from utils import denormalize
 
-if __name__=="__main__":
-    
-    cfg = Config()                                                          # configuration
-    model = CSRNet().to(cfg.device)                                         # model
-    criterion = nn.MSELoss(size_average=False)                              # objective
-    optimizer = torch.optim.Adam(model.parameters(),lr=cfg.lr)              # optimizer
-    train_dataloader = create_train_dataloader(cfg.dataset_root, use_flip=True, batch_size=cfg.batch_size)
-    test_dataloader  = create_test_dataloader(cfg.dataset_root)             # dataloader
 
-    min_mae = sys.maxsize
-    min_mae_epoch = -1
-    for epoch in range(1, cfg.epochs):                          # start training
-        model.train()
-        epoch_loss = 0.0
-        for i, data in enumerate(tqdm(train_dataloader)):
-            image = data['image'].to(cfg.device)
-            gt_densitymap = data['densitymap'].to(cfg.device)
-            et_densitymap = model(image)                        # forward propagation
-            loss = criterion(et_densitymap,gt_densitymap)       # calculate loss
-            epoch_loss += loss.item()
-            optimizer.zero_grad()
-            loss.backward()                                     # back propagation
-            optimizer.step()                                    # update network parameters
-        cfg.writer.add_scalar('Train_Loss', epoch_loss/len(train_dataloader), epoch)
+class CSRNetLightning(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.model = CSRNet()
+        self.criterion = nn.MSELoss(size_average=False)
 
-        model.eval()
-        with torch.no_grad():
-            epoch_mae = 0.0
-            for i, data in enumerate(tqdm(test_dataloader)):
-                image = data['image'].to(cfg.device)
-                gt_densitymap = data['densitymap'].to(cfg.device)
-                et_densitymap = model(image).detach()           # forward propagation
-                mae = abs(et_densitymap.data.sum()-gt_densitymap.data.sum())
-                epoch_mae += mae.item()
-            epoch_mae /= len(test_dataloader)
-            if epoch_mae < min_mae:
-                min_mae, min_mae_epoch = epoch_mae, epoch
-                torch.save(model.state_dict(), os.path.join(cfg.checkpoints,str(epoch)+".pth"))     # save checkpoints
-            print('Epoch ', epoch, ' MAE: ', epoch_mae, ' Min MAE: ', min_mae, ' Min Epoch: ', min_mae_epoch)   # print information
-            cfg.writer.add_scalar('Val_MAE', epoch_mae, epoch)
-            cfg.writer.add_image(str(epoch)+'/Image', denormalize(image[0].cpu()))
-            cfg.writer.add_image(str(epoch)+'/Estimate density count:'+ str('%.2f'%(et_densitymap[0].cpu().sum())), et_densitymap[0]/torch.max(et_densitymap[0]))
-            cfg.writer.add_image(str(epoch)+'/Ground Truth count:'+ str('%.2f'%(gt_densitymap[0].cpu().sum())), gt_densitymap[0]/torch.max(gt_densitymap[0]))
-            
-# %%
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        image, gt_densitymap = batch['image'], batch['densitymap']
+        et_densitymap = self(image)
+        loss = self.criterion(et_densitymap, gt_densitymap)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        image, gt_densitymap = batch['image'], batch['densitymap']
+        et_densitymap = self(image).detach()
+        mae = abs(et_densitymap.data.sum() - gt_densitymap.data.sum())
+        self.log('val_mae', mae)
+        return mae
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.lr)
+        return optimizer
+
+    def train_dataloader(self):
+        return create_train_dataloader(self.config.dataset_root, use_flip=True, batch_size=self.config.batch_size)
+
+    def val_dataloader(self):
+        return create_test_dataloader(self.config.dataset_root)
+
+
+if __name__ == "__main__":
+    cfg = Config()
+    model = CSRNetLightning(cfg)
+
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=cfg.checkpoints,
+        filename='{epoch}-{val_mae:.2f}',
+        monitor='val_mae',
+        mode='min',
+        save_top_k=1,
+    )
+
+    trainer = pl.Trainer(max_epochs=cfg.epochs, gpus=1, callbacks=[checkpoint_callback])
+    trainer.fit(model)
