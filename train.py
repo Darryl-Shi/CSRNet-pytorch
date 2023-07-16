2dd43662c1c6d9ca131a4712c51a9ffe25b58a6e
Darryl-Shi <221474B@student.hci.edu.sg>
2023-07-16 14:09:09 +0800

    auto config tpu devices

--- train.py
+++ train.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
         mode='min',
         save_top_k=1,
       )
-      trainer = pl.Trainer(max_epochs=cfg.epochs, accelerator="tpu", callbacks=[checkpoint_callback], logger=wandb_logger)
+      trainer = pl.Trainer(max_epochs=cfg.epochs, accelerator="tpu", devices="auto", callbacks=[checkpoint_callback], logger=wandb_logger)
       trainer.fit(model)
